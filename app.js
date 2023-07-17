8d64d10d59fae411cf3328e30b3d0a6bd1b33a9a
khoitq-acl <khoitq@americancodelab.com>
2023-07-17 17:05:09 +0700

    Change structure to constructor function

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-let box = document.getElementById("check")
-let form = document.createElement("form")
-
-box.appendChild(form)
-
-function PreList(content, name) {
-    this.content = content
+function MenuItem(name, category) {
     this.name = name
+    this.category = category
 }
 
-let milk = new PreList("1 1/2 cups Milk", "milk")
-let mascarpone = new PreList("1/2 cup mascarpone", "mascarpone")
-let salt = new PreList("1/2 tsp pink Salt", "salt")
-let figs = new PreList("1 lb Black Mission Figs", "figs")
-let sugar = new PreList("1/2 cup Brown Sugar", "sugar")
-let water = new PreList(" 2-4 tbsp water,", "water")
-let cream = new PreList(" 1 1/2 cups heavy cream", "cream")
-let granSugar = new PreList("1/3 granulated sugar", "gransugar")
-let egg = new PreList(" egg York", "egg")
-let juiced = new PreList("1 lemon, Juiced", "juiced")
-let butter = new PreList("2 tbsp butter", "butter")
-let honey = new PreList("1 cup honey roasted pecans,  roughly chopped", "honey")
+function Menu(menuDataInput) {
 
-let arrayList = [milk, mascarpone, salt, figs, sugar, water, cream, granSugar, egg, juiced, butter, honey]
+    this.menuDataInput = menuDataInput;
+    this.items = [];
+    this.form = document.querySelector("#menu");
 
+    this.generateMenuItemsFromMenuDataInput = function () {
 
+        for (let i = 0; i < this.menuDataInput.length; i++) {
+            let dataItem = this.menuDataInput[i];
+            this.items.push(new MenuItem(dataItem.name, dataItem.category));
+        }
 
-function renderList(arr) {
+    }
+}
 
 
-    for (let i = 0; i < arr.length; i++) {
+Menu.prototype.render = function () {
+    let form = this.form;
+
+    for (let i = 0; i < this.items.length; i++) {
+
+        let item = this.items[i];
 
         // if in an object. the value we put through should be a properties of a constructor function.
 
@@ -36,28 +33,60 @@ function renderList(arr) {
         div.classList.add("form-item")
         form.appendChild(div)
 
-        let id = arr[i].name + Math.random().toString();
+        let id = item.name + Math.random().toString();
 
         // generate html checkbox
         let renderPrepList = `
                     <input type="checkbox" name="${id}" id="${id}">
-                    <label for="${id}">${arr[i].content}</label>
+                    <label for="${id}">${item.name}</label>
                     <br>
                 `
         div.innerHTML = renderPrepList;
 
-        let checkBoxStatus = div.querySelector(`input[type="checkbox"]`);
-
-        checkBoxStatus.addEventListener("change", function (event) {
-            if (event.target.checked) {
-                div.classList.add('form-item--is-checked')
-            } else {
-                div.classList.remove('form-item--is-checked')
-            }
-        });
-
     }
 }
 
-renderList(arrayList)
-// console.log(checkBoxStatus)
+Menu.prototype.addEvent = function () {
+    let form = this.form;
+
+    form.addEventListener('click', function (event) {
+        console.dir(event.target);
+        // if type of event.target element is checkbox, we will find closest div tag has class form-item
+        if (event.target.tagName === "INPUT" && event.target.type === 'checkbox') {
+            let div = event.target.closest('div.form-item');
+            if (div) {
+                console.log(div);
+                if (event.target.checked) {
+                    div.classList.add('form-item--is-checked')
+                } else {
+                    div.classList.remove('form-item--is-checked')
+                }
+            }
+        }
+
+    })
+}
+
+Menu.prototype.init = function () {
+    this.generateMenuItemsFromMenuDataInput();
+    this.render();
+    this.addEvent();
+}
+
+let menuData = [
+    { "name": "1 1/2 cups Milk", "category": "milk" },
+    { "name": "1/2 cup mascarpone", "category": "mascarpone" },
+    { "name": "1/2 tsp pink Salt", "category": "salt" },
+    { "name": "1 lb Black Mission Figs", "category": "figs" },
+    { "name": "1/2 cup Brown Sugar", "category": "sugar" },
+    { "name": " 2-4 tbsp water", "category": "water" },
+    { "name": " 1 1/2 cups heavy cream", "category": "cream" },
+    { "name": "1/3 granulated sugar", "category": "gransugar" },
+    { "name": " egg York", "category": "egg" },
+    { "name": "1 lemon, Juiced", "category": "juiced" },
+    { "name": "2 tbsp butter", "category": "butter" },
+    { "name": "1 cup honey roasted pecans,  roughly chopped", "category": "honey" },
+];
+
+let menu = new Menu(menuData);
+menu.init();
